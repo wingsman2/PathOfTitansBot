596a158bbb0623e6550aa54162c1974adb1ca31a
wingsman2 <blah_1212@hotmail.com>
2024-07-12 19:01:24 -0400

    Update PlayerLogoutWebhook.js

--- sys/wh/PlayerLogoutWebhook.js
+++ sys/wh/PlayerLogoutWebhook.js
@@ -1,6 +1,5 @@
 const index = require("../../app.js");
-const config = require('../../config');
-const db = require('better-sqlite3')(config.get('maininfo.db'));
+const db = require('better-sqlite3')(`sys/db/${process.env.db}`);
 
 const { Client, Collection, GatewayIntentBits, Routes, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, Events } = require('discord.js');
 
