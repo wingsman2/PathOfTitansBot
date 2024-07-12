61871f017a9960b67f159ea1374d6cd40c03ffff
wingsman2 <blah_1212@hotmail.com>
2024-07-12 14:20:05 -0400

    Update grow.js

--- sys/misc/grow.js
+++ sys/misc/grow.js
@@ -1,5 +1,6 @@
 // Import database
-const db = require('better-sqlite3')('sys/db/Mesozoic.db');
+const config = require('config');
+const db = require('better-sqlite3')(config.get('maininfo.db'));
 const rconCommandStandalone = require("../rcon/rconCommandStandalone.js")
 
 function grow(data, server) {
