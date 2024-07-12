6e918fbe1430be077f7fe3ad0dd040ed442332bb
wingsman2 <blah_1212@hotmail.com>
2024-07-12 13:07:51 -0400

    Update PlayerChatWebhook.js

--- sys/wh/PlayerChatWebhook.js
+++ sys/wh/PlayerChatWebhook.js
@@ -1,5 +1,5 @@
 const index = require("../../app.js");
-const db = require('better-sqlite3')(`sys/db/Mesozoic.db`);
+const db = require('better-sqlite3')(`sys/db/NyghtPotBot.db`);
 
 // Rcon
 const rconCommandStandalone = require("../rcon/rconCommandStandalone.js")
