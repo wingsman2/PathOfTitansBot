f2e1c66f726b6d23c23fcd352724e7a788f7e9ca
wingsman2 <blah_1212@hotmail.com>
2024-07-17 17:30:42 -0400

    Update linkchannels.js

--- sys/discord/linkchannels.js
+++ sys/discord/linkchannels.js
@@ -1,18 +1,18 @@
 // Import database
 const db = require('better-sqlite3')(`sys/db/${process.env.db}`);
 
-function linkchannels(interaction, webid, activity_channel, combat_channel, admin_channel, combat_channel, report_channel, chat_channel, quest_channel, moneylog_channel, nesting_channel) {
+function linkchannels(interaction, webid, activity_channel, combat_channel, admin_channel, report_channel, chat_channel, quest_channel, moneylog_channel, nesting_channel) {
 
 
     const data = db.prepare(`SELECT * FROM channels WHERE guildId = '${interaction.guildId}' AND webid = '${webid}';`).get();
 
     if (data) {
 
-        const dataX = db.prepare(`UPDATE channels SET activity_channel = ? combat_channel = ? admin_channel = ? combat_channel = ? report_channel = ? chat_channel = ? quest_channel = ? moneylog_channel = ? nesting_channel = ? WHERE guildId = ? AND webid = ?;`).run(activity_channel, combat_channel, admin_channel, combat_channel, report_channel, chat_channel, quest_channel, moneylog_channel, nesting_channel, interaction.guildId, webid);
+        const dataX = db.prepare(`UPDATE channels SET activity_channel = ? combat_channel = ? admin_channel = ? report_channel = ? chat_channel = ? quest_channel = ? moneylog_channel = ? nesting_channel = ? WHERE guildId = ? AND webid = ?;`).run(activity_channel, combat_channel, admin_channel, report_channel, chat_channel, quest_channel, moneylog_channel, nesting_channel, interaction.guildId, webid);
         interaction.reply({content: `${interaction.user} Channels have been updated. Type /listchannels to view your channels.`, ephemeral: true});
 
     } else {
-        const dataX = db.prepare(`INSERT INTO channels (guildId, webid, activity_channel, combat_channel, admin_channel, combat_channel, report_channel, chat_channel, quest_channel, moneylog_channel, nesting_channel) Values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);`).run(interaction.guildId, webid, activity_channel, combat_channel, admin_channel, combat_channel, report_channel, chat_channel, quest_channel, moneylog_channel, nesting_channel);
+        const dataX = db.prepare(`INSERT INTO channels (guildId, webid, activity_channel, combat_channel, admin_channel, report_channel, chat_channel, quest_channel, moneylog_channel, nesting_channel) Values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);`).run(interaction.guildId, webid, activity_channel, combat_channel, admin_channel, report_channel, chat_channel, quest_channel, moneylog_channel, nesting_channel);
         interaction.reply({content: `${interaction.user} Channels have been created. Type /listchannels to view your channels.`, ephemeral: true});
     }
 }
