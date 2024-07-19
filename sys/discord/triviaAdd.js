b64eeefbdb1a866be7ba550b693bd7846abe7447
wingsman2 <blah_1212@hotmail.com>
2024-07-19 11:31:34 -0400

    Update triviaAdd.js

--- sys/discord/triviaAdd.js
+++ sys/discord/triviaAdd.js
@@ -5,8 +5,8 @@ const db = require('better-sqlite3')(`sys/db/${process.env.db}`);
 const { Client, Collection, GatewayIntentBits, Routes, EmbedBuilder } = require('discord.js');
 
 
-function triviaAdd(interaction, question, answer) {
-        db.prepare(`INSERT INTO trivia (question, answer) VALUES (?, ?);`).run(question, answer);
+function triviaAdd(interaction, question, answer, marks) {
+        db.prepare(`INSERT INTO trivia (question, answer, marks) VALUES (?, ?, ?);`).run(question, answer, marks);
         interaction.reply({content: `${interaction.user} Added the question to trivia list.`, ephemeral: true});
         return;
 }
