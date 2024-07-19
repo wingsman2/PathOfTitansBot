d0fcc62f1a3e6f25fa76c46861644d193f2b0ce0
wingsman2 <blah_1212@hotmail.com>
2024-07-19 11:42:44 -0400

    Update app.js

--- app.js
+++ app.js
@@ -178,7 +178,7 @@ app.post(`/${process.env.bot_name}/AdminCommand/:id`, (req, res) => { //
 
 // Trivia
 //schedule.scheduleJob('0 0 * * * *', function(){
-schedule.scheduleJob('*/5 * * * * *', function(){
+schedule.scheduleJob('0 */5 * * * *', function(){
 	triviaGenerate();
 });
 
