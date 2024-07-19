e06a6d3889b8dcd1d9ea8c2230873b36de75890e
wingsman2 <blah_1212@hotmail.com>
2024-07-19 14:50:44 -0400

    Update help.js

--- sys/misc/help.js
+++ sys/misc/help.js
@@ -4,7 +4,15 @@ const rconCommandStandalone = require("../rcon/rconCommandStandalone.js")
 
 function help(data, server) {
 
-    rconCommandStandalone(`whisper ${data.PlayerName} There is no help!`, servers[server-1]);
+    rconCommandStandalone(`whisper ${data.PlayerName} !help - Displays ingame commands`, servers[server-1]);
+    rconCommandStandalone(`whisper ${data.PlayerName} !balance - shows current ERA and marks balances.`, servers[server-1]);
+    rconCommandStandalone(`whisper ${data.PlayerName} !grow <stage/full> - Allows you to buy growth for ERA if your dino is setup in the store.`, servers[server-1]);
+    rconCommandStandalone(`whisper ${data.PlayerName} !farm - command that can be run every 20mins to gain marks if set.`, servers[server-1]);
+    rconCommandStandalone(`whisper ${data.PlayerName} !deposit <value> - Transfer Marks to ERA.`, servers[server-1]);
+    rconCommandStandalone(`whisper ${data.PlayerName} !withdraw <value> - Transfer ERA to Marks.`, servers[server-1]);
+    rconCommandStandalone(`whisper ${data.PlayerName} !accept - accept a nest invitation.`, servers[server-1]);
+    rconCommandStandalone(`whisper ${data.PlayerName} !deny - deny a nest invitation.`, servers[server-1]);
+    rconCommandStandalone(`whisper ${data.PlayerName} !redeem <code> - redeemable codes for dino growth.`, servers[server-1]);
         
 }
 
