06e1e3c68733f500197f56dc6dfd41e8a2e6e55c
wingsman2 <blah_1212@hotmail.com>
2024-07-18 21:35:35 -0400

    Update farmamountset.js

--- commands/farmamountset.js
+++ commands/farmamountset.js
@@ -6,5 +6,12 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('farmamountset')
 		.setDescription('Set the amount /farm gives on each server.')
-
+		.addStringOption(option =>
+			option.setName('webid')
+				.setDescription('The webID of the server provided in /listgameserver')
+				.setRequired(true))
+		.addStringOption(option =>
+			option.setName('amount')
+				.setDescription('Amount of credits')
+				.setRequired(true))
 };
