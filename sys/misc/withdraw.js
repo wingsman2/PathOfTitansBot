212693df4f89e5e495e3c4dd719798b589ba9d13
wingsman2 <blah_1212@hotmail.com>
2024-07-19 21:13:59 -0400

    Update withdraw.js

--- sys/misc/withdraw.js
+++ sys/misc/withdraw.js
@@ -70,7 +70,7 @@ function withdraw(data, server) {
                 });
 
             } else {
-                rconCommandStandalone(`whisper ${data.PlayerName} :pot: Please link your Alderon account to our Discord by using /link in our Discord Server.`, servers[server-1]);
+                rconCommandStandalone(`whisper ${data.PlayerName} :pot: Please link your Alderon account to our Discord by using /link in our Discord Server ${process.env.discordlink}.`, servers[server-1]);
                 return;
             }
         } else {
